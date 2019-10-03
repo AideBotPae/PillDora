851346125013bb8b90be7464b6ed48de8acfab3b
haerba <hamza.fiveplayer@gmail.com>
2019-10-03 11:28:27 +0200

    Server change

--- Server/server_client_test.py
+++ Server/server_client_test.py
@@ -1,30 +1,7 @@
 import socket
-import sys
-
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Connect the socket to the port where the server is listening
-server_address = ('localhost', 8080)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
-sock.connect(server_address)
-
-try:
-
-    # Send data
-    message = 'This is the message.  It will be repeated.'
-    print(sys.stderr, 'sending "%s"' % message)
-    sock.sendall(message)
-
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
-
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print(sys.stderr, 'received "%s"' % data)
-
-finally:
-    print(sys.stderr, 'closing socket')
-    sock.close()
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('0.0.0.0', 8080))
+client.send("I am CLIENT\n")
+from_server = client.recv(4096)
+client.close()
+print from_server
