73b9cd938039832bf4d7b7c656d74951c6d72288
Eloy Marin <eloymc16@gmail.com>
2019-10-03 12:44:32 +0200

    fafa

--- Server/database.py
+++ Server/database.py
@@ -1,32 +1,47 @@
 import pymysql
 
 
-class ClientChecker:
+class DatabaseConnector:
+    @property
+    def connect(self):
+        # Open database connection
+        return pymysql.connect("localhost", "paesav", "12345678", "aidebot")
+
+
+class ClientChecker(DatabaseConnector):
 
     def __init__(self, user_id, passsword):
         self.user_id = user_id
         self.password = passsword
-
-    def run(self):
-        # Open database connection
-        db = pymysql.connect("localhost", "paesav", "12345678", "aidebot")
+        self.user_exists = True
+        self.db = self.connect
 
         # prepare a cursor object using cursor() method
-        cursor = db.cursor()
+        self.cursor = self.db.cursor()
 
+    def check_user(self):
+        self.cursor.execute("SELECT id, password FROM aidebot.users where id={id}".format(id=self.user_id))
+        data = self.cursor.fetchall()
+
+        if not data:
+            print("User isn't registered\n")
+        return False
+
+    def add_user(self):
+        if not check_user:
+
+
+    def check_password(self):
         # execute SQL query using execute() method.
-        cursor.execute("SELECT id, password FROM aidebot.users where id={id}".format(id=self.user_id))
+        self.cursor.execute("SELECT id, password FROM aidebot.users where id={id}".format(id=self.user_id))
 
-        # Fetch a single row using fetchone() method.
-        data = cursor.fetchall()
+        # Fetch all rows using fetchone() method.
+        data = self.cursor.fetchall()
 
-        print("Data ")
-        print(data)
-
-        # disconnect from server
-        db.close()
+        if self.password != data[0][1]:
+            print('Wrong password')
 
 
 if __name__ == "__main__":
     checker = ClientChecker(user_id=1, passsword=12)
-    checker.run()
+    checker.check_password()
