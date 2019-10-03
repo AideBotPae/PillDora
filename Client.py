27c7b8852f665e28a76fee7e28dc8c7e9fe5fb5e
Esteve Valls <43722285+Evm7@users.noreply.github.com>
2019-10-03 12:41:33 +0200

    Multithreading and states array update.

--- Client.py
+++ Client.py
@@ -1,6 +1,19 @@
-from pycparser.ply.lex import TOKEN
+
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+#
+# Simple Bot to reply to Telegram messages
+# This program is dedicated to the public domain under the CC0 license.
+"""
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+
+"""
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler, MessageHandler, Filters, ConversationHandler, \
     CallbackQueryHandler
+from telegram.ext.dispatcher import run_async
+
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 import requests
 import telegram
@@ -15,11 +28,13 @@ logger = logging.getLogger('AideBot')
 
 TOKEN_AIDEBOT = '902984072:AAFd0KLLAinZIrGhQvVePQwBt3WJ1QQQDGs'
 TOKEN_PROVE = '877926240:AAEuBzlNaqYM_kXbOMxs9lzhFsR7UpoqKWQ'
-LOGIN, NEW_USER, CHOOSING, INTR_MEDICINE, CHECK_MED, CHECK_REM , CALENDAR_CHOOSE, CALENDAR_TASKS, GET_CN = range(9)
+LOGIN, NEW_USER, CHOOSING, INTR_MEDICINE, CHECK_MED, CHECK_REM, CALENDAR_TASKS, GET_CN = range(8)
+
+states=[LOGIN, LOGIN]
 
 reply_keyboard = [['Introduce Medicine', 'Calendar'],
                   ['History', 'Delete reminder'],
-                  ['Journey', 'Done']]
+                  ['Journey', 'Exit']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
 yes_no_reply_keyboard = [['YES', 'NO']]
@@ -49,20 +64,26 @@ def user_verification(update, context):
     user_id = update.message.from_user.id
     return True
 
+def set_state(state):
+    states[1]=states[0]
+    states[0]=state
+    return state
 
 # function used to Introduce Password
+@run_async
 def intr_pwd(update, context):
     password = update.message.text
-    logger.info('Password is ' + password)
+    logger.info('Password for user ' +get_name(update.message.from_user)+ ' is ' + password)
     if (pwd_verification(password) == False):
         update.message.reply_text("Wrong Password. Enter correct password again:")
         return LOGIN
     update.message.reply_text('Welcome ' + get_name(update.message.from_user) + '. How can I help you?',
                               reply_markup=markup)
-    return CHOOSING
+    return set_state(CHOOSING)
 
 
 # function used to create user account --> associate UserID with Pwd
+@run_async
 def new_user(update, context):
     password = update.message.text
     # check if password difficulty.
@@ -88,16 +109,17 @@ def new_user(update, context):
                                   reply_markup=markup)
         # Introdue new UserID-Password to DataBase
         x = False
-        return CHOOSING
+        return set_state(CHOOSING)
 
     update.message.reply_text(
         "Not a Valid Password. Enter Password with 6 to 12 characters and minimum 3 of these types of characters: uppercase, lowercase, number and $, # or @")
-    return NEW_USER
+    return set_state(NEW_USER)
 
 
 def choose_function(update, context):
-    logger.info('User in the menu')
+    logger.info('User ' +get_name(update.message.from_user)+ ' in the menu')
     update.message.reply_text("Is there any other way I can help you?", reply_markup=markup)
+    return set_state(CHOOSING)
 
 def start(update, context):
     logger.info('User has connected to AideBot: /start')
@@ -107,37 +129,42 @@ def start(update, context):
     logger.info('Name of user is: ' + name + " and its ID is " + str(user_id))
     if (user_verification(update, context)):
         update.message.reply_text("Enter your password in order to get Assistance:")
-        return LOGIN
+        return set_state(LOGIN)
     else:
         context.bot.send_message(chat_id=update.message.chat_id, text=("Welcome to the HealthCare Assistant AideBot."
                                                                        "Enter new password for creating your account:"))
-    return NEW_USER
+    return set_state(NEW_USER)
 
-
+@run_async
 def intr_medicine(update, context):
-    logger.info('User introducing new medicine')
+    logger.info('User ' +get_name(update.message.from_user)+ ' introducing new medicine')
     update.message.reply_text(
         'Please Introduce New Medicine using next format:\nCodeCN-Quantity-Frequency-EndDate-Expiration Date')
-    return INTR_MEDICINE
+    return set_state(INTR_MEDICINE)
 
 
 def send_new_medicine(update, context):
     medicine = update.message.text.split('-')
-    logger.info(
-        'New medicine.\n\tCN : ' + medicine[0] + '\n\tQuantity : ' + medicine[1] + '\n\tFrequency : ' +
-        medicine[2] + '\n\tEndDate : ' + medicine[3] + '\n\tExpiration Date : ' + medicine[4])
-    update.message.reply_text(
-        'Medicine correctly introduced!\n\tCN : ' + medicine[0] + '\n\tQuantity : ' + medicine[1] + '\n\tFrequency : ' +
-        medicine[2] + '\n\tEndDate : ' + medicine[3] + '\n\tExpiration Date : ' + medicine[4])
-    update.message.reply_text('Is the medicine correctly introduced? ', reply_markup=yes_no_markup)
-    return CHECK_MED
+    if (len(medicine)!=5):
+        logger.info('Error: user ' + get_name(update.message.from_user) + ' did not introduce all fields of new medicine')
+        update.message.reply_text("Warning: complete all the fields as mentioned before.")
+        return intr_medicine(update, context)
+    else:
+        logger.info(
+            'New medicine for user  ' +get_name(update.message.from_user)+ ' .\n\tCN : ' + medicine[0] + '\n\tQuantity : ' + medicine[1] + '\n\tFrequency : ' +
+            medicine[2] + '\n\tEndDate : ' + medicine[3] + '\n\tExpiration Date : ' + medicine[4])
+        update.message.reply_text(
+            'Medicine correctly introduced!\n\tCN : ' + medicine[0] + '\n\tQuantity : ' + medicine[1] + '\n\tFrequency : ' +
+            medicine[2] + '\n\tEndDate : ' + medicine[3] + '\n\tExpiration Date : ' + medicine[4])
+        update.message.reply_text('Is the medicine correctly introduced? ', reply_markup=yes_no_markup)
+        return set_state(CHECK_MED)
 
-
+@run_async
 def see_calendar(update, context):
-    logger.info('User seeing calendar')
+    logger.info('User ' +get_name(update.message.from_user)+ '  seeing calendar')
     update.message.reply_text("Please select a date: ",
                               reply_markup=telegramcalendar.create_calendar())
-
+@run_async
 def inline_handler(update, context):
     selected, date = telegramcalendar.process_calendar_selection(context.bot, update)
     if selected:
@@ -145,53 +172,54 @@ def inline_handler(update, context):
                          text="You selected %s" % (date.strftime("%d/%m/%Y")),
                          reply_markup=ReplyKeyboardRemove())
         get_calendar_tasks(update, context, date.strftime("%d/%m/%Y"))
-    return CHOOSING
+    return set_state(CHOOSING)
 
-
+@run_async
 def get_calendar_tasks(update, context, date):
     logger.info('Tasks for the user on the date '+ date)
     #connects to DataBase with Date and UserId asking for all the tasks of this date
     context.bot.send_message(chat_id=update.callback_query.from_user.id, text="Is there any other way I can help you?",
                              reply_markup=markup)
 
-
+@run_async
 def see_history(update, context):
-    logger.info('User seeing history')
+    logger.info('User ' +get_name(update.message.from_user)+ '  seeing history')
     #connects to DataBase with UserId asking for all the meds currently taking
     user_id = update.message.from_user.id
     return choose_function(update, context)
 
-
+@run_async
 def delete_reminder(update, context):
-    logger.info('User deleting reminder')
+    logger.info('User ' +get_name(update.message.from_user)+ ' deleting reminder')
     update.message.reply_text('Please Introduce CN of the Medicine you want to delete the reminder:')
-    return GET_CN
+    return set_state(GET_CN)
 
 def get_medicine_CN(update, context):
     medicine_CN = update.message.text
     #connects to DataBase with UserId and get the current reminder for this medicine_CN.
     update.message.reply_text('Reminder asked to be removed:\n\tMedicine: \n\tTaken from:\n\tEnd date:\n\tFrequency: ')
     update.message.reply_text('Is this the reminder you want to remove? ', reply_markup=yes_no_markup)
-    return CHECK_REM
+    return set_state(CHECK_REM)
 
-def done(update, context):
+def exit(update, context):
     update.message.reply_text("See you next time")
-    logger.info('User finish with AideBot')
+    logger.info('User ' +get_name(update.message.from_user)+ ' finish with AideBot')
     return ConversationHandler.END
 
-
+@run_async
 def create_journey(update, context):
 
-    logger.info('User creating journey')
+    logger.info('User ' +get_name(update.message.from_user)+ ' creating journey')
     return choose_function(update, context)
 
 
 def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
-    updater = Updater(token=TOKEN_PROVE, use_context=True)
+    updater = Updater(token=TOKEN_PROVE, use_context=True, workers=50)
     dp = updater.dispatcher
     conv_handler = ConversationHandler(
+        allow_reentry=True,
         entry_points=[CommandHandler('start', start)],
 
         states={
@@ -215,11 +243,10 @@ def main():
             CHECK_REM: [MessageHandler(Filters.regex('^YES$'), choose_function),
                     MessageHandler(Filters.regex('^NO$'), delete_reminder)
                     ],
-            CALENDAR_CHOOSE: [MessageHandler(Filters.text, inline_handler)],
             GET_CN: [MessageHandler(Filters.text, get_medicine_CN)]
 
         },
-        fallbacks=[MessageHandler(Filters.regex('^Done$'), done)]
+        fallbacks=[MessageHandler(Filters.regex('^Exit$'), exit)]
 
     )
 
