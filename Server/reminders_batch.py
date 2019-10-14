42e198f82442dad3dbadea4b8a9285fb247a2cab
Eloy Marin <eloymc16@gmail.com>
2019-10-14 11:50:02 +0200

    fafsfefae

--- Server/reminders_batch.py
+++ Server/reminders_batch.py
@@ -18,10 +18,14 @@ def create_reminders():
 
             methods.insert_reminders(user_id=values[0], cn=values[1], date=date_hour)
 
+
 def delete_reminders():
     date = datetime.datetime.utcnow()
     methods.suprimir_reminders(date)
 
+
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
-    scheduler.add_job()
+    scheduler.add_job(func=create_reminders, trigger='cron', hour=1)
+    scheduler.add_job(func=delete_reminders, trigger='cron', hour=2)
+    scheduler.start()
