f4cbca02955382f0b1ee7b40488a389cdf352e9b
Eloy Marin <eloymc16@gmail.com>
2019-10-17 20:03:58 +0200

    reminders new structure

--- Server/database.py
+++ Server/database.py
@@ -48,6 +48,7 @@ class Database(DatabaseConnectionCredentials):
 
 class DBMethods:
 
+    # User table methods
     def check_user(self, user_id):
         with Database() as db:
             data = db.query("SELECT id FROM aidebot.users where id={id}".format(id=user_id))
@@ -85,18 +86,24 @@ class DBMethods:
                 print('Correct password')
                 return True
 
+    # Receipts table methods
     def introd_receipt(self, query_parsed, user_id, date):
         with Database() as db:
-            db.execute('''INSERT INTO aidebot.receipts (user_id,national_code, frequency, quantity, begin_date, end_date)
-                        values ({id},{cn},{frequency},'{quantity}','{init}','{end}')'''.format(
-                id=user_id, cn=query_parsed['NAME'], frequency=query_parsed['FREQUENCY'],
-                quantity=query_parsed['QUANTITY'],
-                init=date,
-                end=query_parsed['END_DATE']
-            ))
-            self.intr_inventory(user_id=user_id, query_parsed=query_parsed)
-            self.create_reminders(user_id=user_id, query_parsed=query_parsed)
-            # Comprobar que se ha introducido bien
+            exists = self.check_receipt(user_id=user_id, cn=query_parsed['NAME'])
+            if not exists:
+                db.execute('''INSERT INTO aidebot.receipts (user_id,national_code, frequency, quantity, begin_date, end_date)
+                            values ({id},{cn},{frequency},'{quantity}','{init}','{end}')'''.format(
+                    id=user_id, cn=query_parsed['NAME'], frequency=query_parsed['FREQUENCY'],
+                    quantity=query_parsed['QUANTITY'],
+                    init=date,
+                    end=query_parsed['END_DATE']
+                ))
+                self.intr_inventory(user_id=user_id, query_parsed=query_parsed)
+                # En la tabla de reminders habría lo mismo que en receipts pero menos columnas, es innecesaria
+                # self.create_reminders(user_id=user_id, query_parsed=query_parsed)
+            else:
+                # TODO: Añadir cantidad de pastillas?
+                return False
 
     def check_receipt(self, cn, user_id):
         with Database() as db:
@@ -125,68 +132,14 @@ class DBMethods:
             else:
                 return False
 
-    ''' 
-    TODO
-    '''
-
-    def intr_inventory(self, user_id, query_parsed):
+    def get_user_receipts_frequency(self, user_id):
         with Database() as db:
-            db.execute('''INSERT INTO aidebot.inventory (user_id,national_code, num_of_pills, expiracy_date)
-                        values ({id},{cn},'{quantity}','{exp_date}')'''.format(id=user_id,
-                                                                               cn=query_parsed['NAME'],
-                                                                               quantity=query_parsed['QUANTITY'],
-                                                                               exp_date=query_parsed['EXP_DATE']
-                                                                               ))
-
-    def get_receipts(self, user_id, cn):
-        with Database() as db:
-            data = db.query(''' SELECT national_code, frequency,end_date
+            data = db.query(''' SELECT national_code,frequency
              FROM aidebot.receipts 
-             WHERE user_id={id} and national_code={cn}
-            '''.format(cn=cn, id=user_id
+             WHERE user_id={id}
+            '''.format(id=user_id
                        ))
             return data
-
-    def create_reminders(self, user_id, query_parsed):
-        with Database() as db:
-            db.execute('''INSERT INTO aidebot.daily_reminders (user_id, national_code, frequency)
-                                    values ({id},{cn},'{frequency}')'''.format(id=user_id,
-                                                                               cn=query_parsed['NAME'],
-                                                                               frequency=query_parsed[
-                                                                                   'FREQUENCY'],
-                                                                               ))
-
-    def get_reminders(self, user_id, date, to_date=None, cn=None):
-        with Database() as db:
-            if to_date:
-                data = db.query(''' SELECT national_code, date
-                FROM aidebot.receipts
-                WHERE begin_date>='{date}' and end_date<='{to_date}' and user_id={id}
-                '''.format(date=date, to_date=to_date, id=user_id))
-                return data
-            elif cn:
-                if(self.check_receipt(self, cn=cn, user_id=user_id)):
-                    return self.get_receipts(cn, user_id)
-                else:
-                    return "False"
-
-            else:
-                data = db.query('''SELECT national_code, frequency
-                FROM aidebot.daily_reminders 
-                WHERE frequency ='{date}' and user_id={id}
-                '''.format(date=date, id=user_id))
-                return data
-
-    def delete_reminders(self, user_id, national_code):
-        with Database() as db:
-            db.execute('''DELETE FROM aidebot.inventory WHERE user_id={id} and national_code={cn}
-            '''.format(id=user_id, cn=national_code))
-            db.execute('''DELETE FROM aidebot.daily_reminders WHERE user_id={id} and national_code={cn}
-            '''.format(id=user_id, cn=national_code))
-            db.execute('''DELETE FROM aidebot.receipts WHERE user_id={id} and national_code={cn}
-                        '''.format(id=user_id, cn=national_code))
-            # Comprobar si se ha hecho bien
-            return True
 
     def get_history(self, user_id):
         with Database() as db:
@@ -196,37 +149,98 @@ class DBMethods:
                 '''.format(id=user_id))
             return data
 
-    def daily_table(self, user_id, national_code):
-        return False
-
-    '''
-    Reminders batch job methods!
-    '''
-
-    def get_all_receipts(self):
+    # Inventory table methods
+    def intr_inventory(self, user_id, query_parsed):
+        # Quantity es la cantidad que ha de tomarse, no las pastillas que hay
         with Database() as db:
-            data = db.query('''SELECT *
-            FROM aidebot.receipts
-            ''')
-            return data
+            db.execute('''INSERT INTO aidebot.inventory (user_id,national_code, num_of_pills, expiracy_date)
+                        values ({id},{cn},'{quantity}','{exp_date}')'''.format(id=user_id,
+                                                                               cn=query_parsed['NAME'],
+                                                                               quantity=0,  # Hay que pedirlo
+                                                                               exp_date=query_parsed['EXP_DATE']
+                                                                               ))
 
-    def insert_reminders(self, user_id, cn, date):
+    # Reminders methods
+    def get_reminders(self, user_id, date, to_date=None, cn=None):
         with Database() as db:
-            data = db.query('''SELECT *
-            FROM aidebot.daily_reminders
-            WHERE user_id={id} and national_code = {cn} and cast(frequency as frequency) = '{date}'
-            '''.format(id=user_id, cn=cn, date=date))
-            if data:
-                db.execute('''INSERT INTO aidebot.daily_reminders (user_id, national_code, frequency)
-                values ({id},{national_code},'{date}')            
-                '''.format(id=user_id, national_code=cn, date=date))
+            if to_date:
+                # In this case, we want to know beforehand the amount of times that the user will have to take their
+                # medicines
 
-    def suprimir_reminders(self, date):
+                # TODO: Como me pasan las fechas para calcular dias de viaje
+                days_away = (to_date - date).days
+
+                data = db.query(''' SELECT national_code, frequency
+                FROM aidebot.receipts
+                WHERE user_id={id}
+                '''.format(id=user_id))
+
+                journey_info = {}
+                for values in data:
+                    national_code = values[0]
+                    frequency = values[1]
+                    num_to_take = frequency * days_away
+                    journey_info[national_code] = {}
+                    journey_info[national_code]['frequency'] = frequency
+                    journey_info[national_code]['quantity_to_take'] = num_to_take
+
+                return journey_info
+
+            elif cn:
+                if self.check_receipt(cn=cn, user_id=user_id):
+                    return self.get_medicine_frequency(user_id=user_id, cn=cn)
+                else:
+                    return "False"
+
+            else:
+                data = self.get_user_receipts_frequency(user_id=user_id)
+                return data
+
+    def delete_reminders(self, user_id, national_code):
         with Database() as db:
-            db.execute('''DELETE FROM aidebot.daily_reminders WHERE frequency<'{date}'
-            '''.format(date=date))
-            # Comprobar si se ha hecho bien
+            db.execute('''DELETE FROM aidebot.inventory WHERE user_id={id} and national_code={cn}
+            '''.format(id=user_id, cn=national_code))
+            # db.execute('''DELETE FROM aidebot.daily_reminders WHERE user_id={id} and national_code={cn}
+            # '''.format(id=user_id, cn=national_code))
+            db.execute('''DELETE FROM aidebot.receipts WHERE user_id={id} and national_code={cn}
+                        '''.format(id=user_id, cn=national_code))
             return True
+
+    # def create_reminders(self, user_id, query_parsed):
+    #     with Database() as db:
+    #         db.execute('''INSERT INTO aidebot.daily_reminders (user_id, national_code, frequency)
+    #                                    values ({id},{cn},'{frequency}')'''.format(id=user_id,
+    #                                                                               cn=query_parsed['NAME'],
+    #                                                                               frequency=query_parsed[
+    #                                                                                   'FREQUENCY'],
+    #                                                                               ))
+
+    # Reminders batch job methods
+
+    # def get_all_receipts(self):
+    #     with Database() as db:
+    #         data = db.query('''SELECT *
+    #         FROM aidebot.receipts
+    #         ''')
+    #         return data
+    #
+    # def insert_reminders(self, user_id, cn, date):
+    #     with Database() as db:
+    #         data = db.query('''SELECT *
+    #         FROM aidebot.daily_reminders
+    #         WHERE user_id={id} and national_code = {cn} and cast(frequency as frequency) = '{date}'
+    #         '''.format(id=user_id, cn=cn, date=date))
+    #         if not data:
+    #             db.execute('''INSERT INTO aidebot.daily_reminders (user_id, national_code, frequency)
+    #             values ({id},{national_code},'{date}')
+    #             '''.format(id=user_id, national_code=cn, date=date))
+    #
+    # def suprimir_reminders(self, date):
+    #     with Database() as db:
+    #         db.execute('''DELETE FROM aidebot.daily_reminders WHERE frequency<'{date}'
+    #         '''.format(date=date))
+    #         # Comprobar si se ha hecho bien
+    #         return True
 
 
 if __name__ == "__main__":
