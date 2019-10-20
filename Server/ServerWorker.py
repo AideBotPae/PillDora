d73d979bcaa686421efc59a025a5a9ecad8c82e0
Esteve Valls <43722285+Evm7@users.noreply.github.com>
2019-10-20 13:12:30 +0200

    Update ServerWorker.py

--- Server/ServerWorker.py
+++ Server/ServerWorker.py
@@ -171,28 +171,3 @@ class ServerWorker:
             response = self.bot_parser("ALL", "DAILY REMINDER") + '"reminder_info" : "' + reminder_info + '"}}'
             self.logger.info(response)
             return response
-
-    def json_query_comprovar(self, query):
-        query_1 = """{
-                "glossary": {
-                    "title": "example glossary",
-                    "GlossDiv": {
-                        "title": "S",
-                        "GlossList": {
-                            "GlossEntry": {
-                                "ID": "SGML",
-                                "SortAs": "SGML",
-                                "GlossTerm": "Standard Generalized Markup Language",
-                                "Acronym": "SGML",
-                                "Abbrev": "ISO 8879:1986",
-                                "GlossDef": {
-                                    "para": "A meta-markup language, used to create markup languages such as DocBook.",
-                                    "GlossSeeAlso": ["GML", "XML"]
-                                },
-                                "GlossSee": "markup"
-                            }
-                        }
-                    }
-                }
-        }"""
-        query_jsoned = json.dumps()
