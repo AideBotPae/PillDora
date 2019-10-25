876bc129f36e00551bb357e4b20e26010d312406
Esteve Valls <43722285+Evm7@users.noreply.github.com>
2019-10-25 17:47:52 +0200

    Update bot.py

--- botui/bot.py
+++ botui/bot.py
@@ -627,8 +627,11 @@ class PillDora:
         self.set_query(user_id, ["user_id"], [str(user_id)])
         query = self.create_query(user_id)
         response = json.loads(self.send_query(user_id, query))
-        update.message.reply_text(
-            "To sum up, your inventory consists on:\n" + response['parameters']['inventory'])
+        if response['parameters']['inventory'] =="False":
+            update.message.reply_text("There is actually no Inventory about you in the DataBase")
+        else:
+            update.message.reply_text(
+                "To sum up, your inventory consists on:\n" + response['parameters']['inventory'])
         self.set_query(user_id, ["None"], ["None"])
         return self.manage_response(update, context)
 
