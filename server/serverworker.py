2882b4972768484fd5712e31095a2a2867ae458c
Esteve Valls <43722285+Evm7@users.noreply.github.com>
2019-10-26 13:05:57 +0200

    Update serverworker.py

--- server/serverworker.py
+++ server/serverworker.py
@@ -89,6 +89,8 @@ class ServerWorker:
                     total_in_inventory += values[1]
                 days_between=(datetime.datetime.strptime(parsed_string["parameters"]["END_DATE"], "%Y-%m-%d") -datetime.datetime.now()).days
                 total_needed=int(parsed_string["parameters"]["QUANTITY"])*24/int(parsed_string["parameters"]["FREQUENCY"])*days_between
+                self.logger.info("Days between: "+ days_between+ "\t total_in_inv="+ total_in_inventory +"\ttotal needed= "+ total_needed)
+
                 if total_in_inventory>=total_needed:
                     param += ',"inventory":"Enough"'
                 else:
