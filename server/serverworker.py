741b3aa6af626b3c9a4e23ab0ec9249bfcd125a0
Esteve Valls <43722285+Evm7@users.noreply.github.com>
2019-10-26 12:49:42 +0200

    Update serverworker.py

--- server/serverworker.py
+++ server/serverworker.py
@@ -62,28 +62,40 @@ class ServerWorker:
             user_id = parsed_string["user_id"]
             national_code = parsed_string["parameters"]["NAME"]
             is_there = self.checker.check_receipt(user_id=user_id, cn=national_code)
+            param = ""
             if not is_there:
+                #No more prescriptions with same CN
                 self.checker.introd_receipt(user_id=user_id, query_parsed=parsed_string["parameters"],
                                             date=datetime.date.today().strftime("%Y-%m-%d"))
-                response = self.bot_parser(user_id=user_id, function="INTRODUCE PRESCRIPTION") + """ "Code": "0"}}"""
+                param = ' "Code": "0"'
 
-                self.logger.info(response)
-                return response
             elif not self.checker.check_medicine_frequency(user_id=user_id, cn=national_code,
                                                            freq=parsed_string["parameters"]["FREQUENCY"]):
+                #Another prescriptions with same CN different frequency
+                param = '"Code": "1" , "freq_database" : "' + str(self.checker.get_medicine_frequency(user_id=user_id,
+                            cn=national_code)) + '", "freq_introduced" : "' + str(parsed_string["parameters"]["FREQUENCY"]) + '"'
 
-                response = self.bot_parser(user_id=user_id,
-                                           function="INTRODUCE PRESCRIPTION") + '"Code": "1" , "freq_database" : "' + str(
-                    self.checker.get_medicine_frequency(user_id=user_id,
-                                                        cn=national_code)) + '", "freq_introduced" : "' + str(
-                    parsed_string["parameters"]["FREQUENCY"]) + '"}}'
-                self.logger.info(response)
-                return response
             else:
-                response = self.bot_parser(user_id=user_id, function=
-                "INTRODUCE PRESCRIPTION") + '"Code" : "2"}}'
-                self.logger.info(response)
-                return response
+                #Same prescriptions already in DB
+                param = '"Code" : "2"'
+
+            data = self.checker.get_cn_from_inventory(user_id=user_id, cn=national_code)
+            if data is ():
+                #User does not have this medicine in its DB inventory
+                param += ',"inventory":"None"'
+            else:
+                total_in_inventory = 0
+                for values in data:
+                    total_in_inventory += values[1]
+                days_between=(datetime.datetime.strptime(parsed_string["parameters"]["end_date"], "%Y-%m-%d") -datetime.datetime.now()).days
+                total_needed=parsed_string["parameters"]["quantity"]*24/parsed_string["parameters"]["frequency"]*days_between
+                if total_in_inventory>=total_needed:
+                    param += ',"inventory":"Enough"'
+                else:
+                    param+=',"inventory":"Need to buy"'
+            response = self.bot_parser(user_id=user_id, function="INTRODUCE PRESCRIPTION") + param + '}}'
+            self.logger.info(response)
+            return response
 
         # INTRODUCING NEW MEDICINE BOUGHT:
         elif instruction == "INTRODUCE MEDICINE":
@@ -101,10 +113,13 @@ class ServerWorker:
                                      parsed_string["parameters"]["arrival_date"]]
             # IF THE BEGINNING DAT AND THE END DATE CONFLICTS, THE METHOD WILL RETURN A NULL CALENDAR OUTPUT
             calendar_output = self.checker.get_reminders(user_id=user_id, date=begin, to_date=end)
-            if calendar_output is not None:
+            if calendar_output is None:
                 journey_info = "Quantity of meds to take:\\n"
                 for output in list(calendar_output.keys()):
-                    journey_info += "\\t-> " + cima.get_med_name(str(output)) + " : " + str(calendar_output[output]) + "\\n"
+                    journey_info += "\\t-> " + cima.get_med_name(str(output)) + " : " + str(
+                        calendar_output[output]) + "\\n"
+            else:
+                journey_info= "No meds need to be taken as there is no prescription for these dates."
             response = self.bot_parser(user_id=user_id,
                                        function="JOURNEY") + '"journey_info" : "' + journey_info + '"}}'
             self.logger.info(response)
@@ -118,7 +133,7 @@ class ServerWorker:
             if calendar_output is not None:
                 journey_info = "Reminders:\\n"
                 for output in calendar_output:
-                    time=str(output[1]).split(',')[1] if len(str(output[1]).split(','))==2 else str(output[1])
+                    time = str(output[1]).split(',')[1] if len(str(output[1]).split(',')) == 2 else str(output[1])
                     journey_info += "\\t-> " + cima.get_med_name(str(output[0])) + " : " + time + "\\n"
             response = self.bot_parser(user_id, "TASKS CALENDAR") + '"tasks" : "' + journey_info + '"}}'
             self.logger.info(response)
@@ -141,9 +156,10 @@ class ServerWorker:
             if current_treatment is not ():
                 current_treatment_info = "Meds currently being taken :\\n"
                 for output in current_treatment:
-                    current_treatment_info += "\\t-> Taking " + cima.get_med_name(str(output[0])) + " until the date of " + str(output[1]).split()[0] + "\\n"
+                    current_treatment_info += "\\t-> Taking " + cima.get_med_name(
+                        str(output[0])) + " until the date of " + str(output[1]).split()[0] + "\\n"
             else:
-                current_treatment_info="False"
+                current_treatment_info = "False"
             response = self.bot_parser(user_id=user_id,
                                        function="CURRENT TREATMENT") + '"reminder_info" : "' + current_treatment_info + '"}}'
             self.logger.info(response)
@@ -158,11 +174,11 @@ class ServerWorker:
                 for output in history:
                     history_info += "\\t-> " + cima.get_med_name(str(output[0])) + " of " + str(output[1])
                     if output[2]:
-                        history_info +=": taken\\n"
+                        history_info += ": taken\\n"
                     else:
                         history_info += ": not taken\\n"
             else:
-                history_info="False"
+                history_info = "False"
             response = self.bot_parser(user_id=user_id,
                                        function="HISTORY") + '"history" : "' + history_info + '"}}'
             self.logger.info(response)
@@ -185,10 +201,11 @@ class ServerWorker:
                 inventory_info = "Your current inventory consists on:\\n"
                 for output in inventory:
                     print(output)
-                    inventory_info += "\\t-> There are " + str(output[1]) + " of " + cima.get_med_name(str(output[0])) + " which expire on " + datetime.datetime.strftime(output[2],
-                                                                                         "%Y-%m-%d")
+                    inventory_info += "\\t-> There are " + str(output[1]) + " of " + cima.get_med_name(
+                        str(output[0])) + " which expire on " + datetime.datetime.strftime(output[2],
+                                                                                           "%Y-%m-%d")
             else:
-                inventory_info="False"
+                inventory_info = "False"
             response = self.bot_parser(user_id=user_id,
                                        function="INVENTORY") + '"inventory" : "' + inventory_info + '"}}'
             self.logger.info(response)
