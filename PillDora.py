325c28780440c1ad3dd827d92a55c6a52806763b
Eloy Marin <eloymc16@gmail.com>
2019-10-28 11:07:42 +0100

    nada

--- PillDora.py
+++ PillDora.py
@@ -1,12 +1,16 @@
 from botui.bot import PillDora
 from server.reminders import Reminder
 import threading
+from multiprocessing.dummy import Manager
+
 
 def run_threaded(job_func):
     job_thread = threading.Thread(target=job_func)
     job_thread.start()
 
+
 if __name__ == "__main__":
-    pilldora= PillDora()
+    q = Manager().Queue()
+    pilldora = PillDora()
     run_threaded(Reminder(pilldora).daily_actualizations)
     pilldora.main()
