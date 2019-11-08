330ba7c02bce318f7c5e2ccd3aaa3ca9a02089b1
Esteve Valls <43722285+Evm7@users.noreply.github.com>
2019-11-08 22:03:40 +0100

    Update bot.py

--- botui/bot.py
+++ botui/bot.py
@@ -467,6 +467,8 @@ class PillDora:
                                      text='Is the medicine correctly introduced? ', reply_markup=yes_no_markup)
             context.bot.send_message(chat_id=user_id,
                                      text=self.show_prescription(user_id), parse_mode=telegram.ParseMode.MARKDOWN)
+            print(list(self.get_prescription(user_id).keys()).append('NAME'))
+            print(list(self.get_prescription(user_id).values()).append(cima.get_med_name(self.get_prescription(user_id)['NAME'])))
             self.set_query(user_id, list(self.get_prescription(user_id).keys()).append('NAME'),
                            list(self.get_prescription(user_id).values()).append(cima.get_med_name(self.get_prescription(user_id)['NAME'])))
             self.set_function(user_id, 'INTRODUCE PRESCRIPTION')
