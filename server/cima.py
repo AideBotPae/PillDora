b9f68992d3b57a52e9576a0a6217b99f53c2114f
Esteve Valls <43722285+Evm7@users.noreply.github.com>
2019-11-10 22:19:37 +0100

    Update cima.py

--- server/cima.py
+++ server/cima.py
@@ -66,11 +66,14 @@ def get_num_pills_nq(data, CN):
 
 
 def get_info_about(CN):
-    data = get_json(CN)
-    estrink = 'El nombre del medicamento es '+ get_med_name_nq(data, CN) + '.\n'
-    estrink += 'Contiene  dosis de '+ data['dosis'] + '.\n'
-    estrink += 'Si desea mas informacion, puede consultar el prospecto en '+ data['docs'][1]['urlHtml']
-    return estrink
+    try:
+        data = get_json(CN)
+        estrink = 'El nombre del medicamento es '+ get_med_name_nq(data, CN) + '.\n'
+        estrink += 'Contiene  dosis de '+ data['dosis'] + '.\n'
+        estrink += 'Si desea mas informacion, puede consultar el prospecto en '+ data['docs'][1]['urlHtml']
+        return estrink
+    except NameError as ex:
+        return "No information could be retrieved from CIMA's server as Connection is Down."
 
 if __name__ == '__main__':
     # CN = input('Introduzca el CN del medicamento ')
